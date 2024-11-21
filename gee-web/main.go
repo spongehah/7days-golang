87a25282277a46457fdc46c412de39ce61563ce2
spongehah <2635879218@qq.com>
2024-11-21 11:15:13 +0800

    Route Group Control

--- gee-web/main.go
+++ gee-web/main.go
@@ -7,23 +7,33 @@ import (
 
 func main() {
 	r := gee.New()
-	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	r.GET("/index", func(ctx *gee.Context) {
+		ctx.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
 
-	r.GET("/hello", func(c *gee.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(ctx *gee.Context) {
+			ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+		v1.GET("/hello", func(ctx *gee.Context) {
+			ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Query("name"), ctx.Path)
+		})
+	}
 
-	r.GET("/hello/:name", func(c *gee.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(ctx *gee.Context) {
+			ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Params["name"], ctx.Path)
+		})
 
-	r.GET("/assets/*filepath", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
-	})
+		v2.POST("/login", func(ctx *gee.Context) {
+			ctx.JSON(http.StatusOK, gee.H{
+				"username": ctx.PostForm("username"),
+				"password": ctx.PostForm("password"),
+			})
+		})
+	}
 
 	r.Run(":9999")
 }
